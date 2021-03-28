befe7340843996a21084a4e2ba79080689f41b1b
newbe36524 <newbe36524@qq.com>
2021-03-28 12:40:55 +0800

    ??

--- src/tasks.py
+++ src/tasks.py
@@ -4,12 +4,19 @@ import json
 import os
 import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-@task
-def sync_cli(c):
-    base_dir = "../release/cli"
-    release_json_path = f'{base_dir}/releases.json'
-    urllib.request.urlretrieve("https://api.github.com/repos/dapr/cli/releases", filename=release_json_path)
+
+def download_releases(base_dir, repo, target_dir):
+    if not base_dir:
+        base_dir = f"../release"
+    asset_dir = f"{base_dir}/{target_dir}"
+    if not os.path.exists(asset_dir):
+        os.makedirs(asset_dir)
+
+    release_json_path = f'{asset_dir}/releases.json'
+    urllib.request.urlretrieve(f"https://api.github.com/repos/{repo}/releases", filename=release_json_path)
     with open(release_json_path) as json_file:
         tags = json.load(json_file)
 
@@ -21,7 +28,17 @@ def sync_cli(c):
             asset_path = f"{tag_dir}/{asset['name']}"
             if not os.path.exists(asset_path):
                 download_url = asset['browser_download_url']
-                logging.info(f"start to request {download_url}")
+                logger.info(f"start to request {download_url}")
                 urllib.request.urlretrieve(download_url, filename=asset_path)
             else:
-                logging.debug(f"skip to download {asset_path}")
+                logger.debug(f"skip to download {asset_path}")
+
+
+@task
+def sync_cli(c, base_dir):
+    download_releases(base_dir, "dapr/cli", "cli")
+
+
+@task
+def sync_runtime(c, base_dir):
+    download_releases(base_dir, "dapr/dapr", "dapr")
